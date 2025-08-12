3bbaed9f7ec8c1c2d0a0effa495912c294f52a46
beriloqueiroz-eco <luiz.queiroz@economatica.com.br>
2025-08-12 12:48:28 -0300

    adjusted compose compose and init grafana config

--- test/main.go
+++ test/main.go
@@ -4,9 +4,10 @@ import "net/http"
 
 // requests
 func main() {
-	go http.DefaultClient.Get("http://localhost:8080/hello")
-	go http.DefaultClient.Get("http://localhost:8080/hello")
-	for i := 0; i < 10; i++ {
-		http.DefaultClient.Get("http://localhost:8080/hello")
+	go http.DefaultClient.Get("http://localhost:8081/hello")
+	go http.DefaultClient.Get("http://localhost:8081/hello")
+	go http.DefaultClient.Get("http://localhost:8082/hello")
+	for i := 0; i < 4; i++ {
+		http.DefaultClient.Get("http://localhost:8083/hello")
 	}
 }
